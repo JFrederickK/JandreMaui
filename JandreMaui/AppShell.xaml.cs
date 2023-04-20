32f3eeae2da94712decf8b058bd777a8de10deca
Jandre Kruger <JandreK@gobid.co.za>
2023-04-20 14:16:13 +0200

    Add project files.

---
+++ JandreMaui/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace JandreMaui;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ JandreMaui/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace JandreMaui;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
